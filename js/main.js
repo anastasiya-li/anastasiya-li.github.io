230c1b8358232eb8fe9bc2020db2040d09ed28a0
anastasiya-li <60786022+anastasiya-li@users.noreply.github.com>
2020-06-14 13:00:12 +0500

    файлы для сайта

---
+++ js/main.js
@@ -0,0 +1,39 @@
+
+$(function(){
+
+    $('.slider__inner').slick({
+      arrows:true,
+      adaptiveHeight: true
+    });
+
+    // $('.works__inner').slick({
+    //     slidesToShow: 4,
+    //     slidesToScroll: 1,
+    //     prevArrow: '<button class="slick-arrow slick-prev" ><img src="img/slider/arrow-left.svg" alt=""></button>',
+    //     nextArrow: '<button class="slick-arrow slick-next" ><img src="img/slider/arrow-right.svg" alt=""></button>',
+    //     responsive: [
+    //       {
+    //         breakpoint: 911,
+    //         settings: {
+    //           slidesToShow: 3,
+    //         }
+    //       },
+    //       {
+    //         breakpoint: 681,
+    //         settings: {
+    //           slidesToShow: 2,
+    //         }
+    //       },
+    //       {
+    //         breakpoint: 455,
+    //         settings: {
+    //           slidesToShow: 1,
+    //         }
+    //       },
+    //     ]
+    //   });
+
+    $('input').styler();
+
+
+  });
